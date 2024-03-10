75d31b2f4bc63ea8287c5dfe0ab0ee6587cc452c
oscar <oscarbz0609@gmail.com>
2024-03-10 14:32:25 -0300

    actualización de vista de mapas con hotreload

--- lib/src/map/map.dart
+++ lib/src/map/map.dart
@@ -1,12 +1,11 @@
+import 'dart:async';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
-import 'package:url_launcher/url_launcher.dart';
-import 'package:syncfusion_flutter_maps/maps.dart';
 import 'dart:convert';
+import 'package:syncfusion_flutter_maps/maps.dart';
 import 'package:location/location.dart';
 
 class MapWidget extends StatefulWidget {
@@ -16,7 +15,7 @@ class MapWidget extends StatefulWidget {
   _MapWidgetState createState() => _MapWidgetState();
 }
 
-//Clase para crear polígonos personalizados
+// Clase para crear polígonos personalizados
 class CustomPolygon {
   final List<MapLatLng> points;
   final Color color;
@@ -40,16 +39,43 @@ class _MapWidgetState extends State<MapWidget> {
 
   List<dynamic> geojson = [];
 
-  //Variables a usar con syncfusion_flutter_maps
+  // Variables a usar con syncfusion_flutter_maps
   late MapZoomPanBehavior _zoomPanBehavior;
   List<CustomPolygon> _polygons = [];
+
+  // Controlador del flujo de datos de polígonos
+  StreamController<List<CustomPolygon>> _polygonStreamController =
+      StreamController<List<CustomPolygon>>();
 
   @override
   void initState() {
     _zoomPanBehavior = MapZoomPanBehavior(enableDoubleTapZooming: true);
     super.initState();
-    fetchPolygons();
-    _currentLocation();
+    _currentLocation().then((locationData) {
+      if (locationData != null) {
+        setState(() {
+          currentLocation =
+              LatLng(locationData.latitude!, locationData.longitude!);
+        });
+      }
+    });
+    // Iniciar el flujo de datos de polígonos
+    _startPolygonStream();
+  }
+
+  @override
+  void dispose() {
+    _tapGestureRecognizer.dispose();
+    _polygonStreamController.close(); // Cerrar el controlador de flujo
+    super.dispose();
+  }
+
+  // Inicia el flujo de datos de polígonos
+  void _startPolygonStream() {
+    Timer.periodic(Duration(seconds: 10), (timer) async {
+      // Actualizar polígonos cada 10 segundos
+      fetchPolygons();
+    });
   }
 
   void fetchPolygons() async {
@@ -77,9 +103,8 @@ class _MapWidgetState extends State<MapWidget> {
         );
         polygons.add(polygon);
       }
-      setState(() {
-        _polygons = polygons;
-      });
+      // Agregar polígonos al flujo de datos
+      _polygonStreamController.sink.add(polygons);
     } else {
       throw Exception('Failed to fetch polygons from API');
     }
@@ -88,39 +113,6 @@ class _MapWidgetState extends State<MapWidget> {
   MapController mapController = MapController();
   LatLng currentLocation = LatLng(-27.332474952498472, -55.864316516887556);
   bool showMarker = false;
-
-  // Future<void> _getLocation() async {
-  //   bool serviceEnabled;
-  //   LocationPermission permission;
-
-  //   serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  //   if (!serviceEnabled) {
-  //     return;
-  //   }
-
-  //   permission = await Geolocator.checkPermission();
-  //   if (permission == LocationPermission.denied) {
-  //     permission = await Geolocator.requestPermission();
-  //     if (permission == LocationPermission.denied) {
-  //       return;
-  //     }
-  //   }
-
-  //   if (permission == LocationPermission.deniedForever) {
-  //     return;
-  //   }
-
-  //   Position position = await Geolocator.getCurrentPosition();
-  //   setState(() {
-  //     showMarker = true;
-  //     currentLocation = LatLng(position.latitude, position.longitude);
-
-  //     // mapController.move(currentLocation, 15.0);
-  //     _zoomPanBehavior
-  //       ..focalLatLng = const MapLatLng(currentLocation)
-  //       ..zoomLevel = 4;
-  //   });
-  // }
 
   Future<LocationData?> _currentLocation() async {
     bool serviceEnabled;
@@ -149,32 +141,39 @@ class _MapWidgetState extends State<MapWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SfMaps(
-        layers: [
-          MapTileLayer(
-            initialFocalLatLng:
-                //MapLatLng(-27.332474952498472, -55.864316516887556),
-                MapLatLng(
-                    currentLocation.latitude!, currentLocation.longitude!),
-            initialZoomLevel: 15,
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            zoomPanBehavior: _zoomPanBehavior,
-            sublayers: [
-              MapPolygonLayer(
-                polygons: _polygons
-                    .map((polygon) => MapPolygon(
-                          points: polygon.points,
-                          color: polygon.color,
-                          strokeColor: polygon.strokeColor,
-                          onTap: () {
-                            _showPolygonInfo(context, polygon);
-                          },
-                        ))
-                    .toSet(),
+      body: StreamBuilder<List<CustomPolygon>>(
+        stream: _polygonStreamController.stream,
+        initialData: _polygons,
+        builder: (BuildContext context,
+            AsyncSnapshot<List<CustomPolygon>> snapshot) {
+          return SfMaps(
+            layers: [
+              MapTileLayer(
+                initialFocalLatLng: MapLatLng(
+                  currentLocation.latitude!,
+                  currentLocation.longitude!,
+                ),
+                initialZoomLevel: 15,
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                zoomPanBehavior: _zoomPanBehavior,
+                sublayers: [
+                  MapPolygonLayer(
+                    polygons: snapshot.data!
+                        .map((polygon) => MapPolygon(
+                              points: polygon.points,
+                              color: polygon.color,
+                              strokeColor: polygon.strokeColor,
+                              onTap: () {
+                                _showPolygonInfo(context, polygon);
+                              },
+                            ))
+                        .toSet(),
+                  ),
+                ],
               ),
             ],
-          ),
-        ],
+          );
+        },
       ),
       floatingActionButton: Stack(
         children: [
@@ -196,25 +195,7 @@ class _MapWidgetState extends State<MapWidget> {
     );
   }
 
-  @override
-  void dispose() {
-    _tapGestureRecognizer.dispose();
-    super.dispose();
-  }
-
-  List<LatLng> parseGeoJSON(String geojsonData) {
-    Map<String, dynamic> geojson = jsonDecode(geojsonData);
-    if (geojson != null &&
-        geojson['coordinates'] != null &&
-        geojson['coordinates'][0] != null) {
-      List<dynamic> coordinates = geojson['coordinates'][0];
-      return coordinates.map((coord) => LatLng(coord[1], coord[0])).toList();
-    } else {
-      return [];
-    }
-  }
-
-  // Funcion para mostrar información del polígono al hacer click
+  // Función para mostrar información del polígono al hacer clic
   void _showPolygonInfo(BuildContext context, CustomPolygon polygon) {
     showModalBottomSheet(
       context: context,
@@ -246,170 +227,3 @@ class _MapWidgetState extends State<MapWidget> {
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_map/flutter_map.dart';
-// import 'package:syncfusion_flutter_maps/maps.dart';
-// import 'package:latlong2/latlong.dart';
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
-// import 'package:flutter/gestures.dart';
-
-// import 'package:geolocator/geolocator.dart';
-
-// import 'package:url_launcher/url_launcher.dart';
-
-// class MapWidget extends StatefulWidget {
-//   const MapWidget({Key? key});
-
-//   @override
-//   _MapWidgetState createState() => _MapWidgetState();
-// }
-
-// class _MapWidgetState extends State<MapWidget> {
-//   late Stream<List<CustomPolygon>> polygonsStream;
-
-//   //Variables a usar con syncfusion_flutter_maps
-//   late MapZoomPanBehavior _zoomPanBehavior;
-//   List<CustomPolygon> _polygons = [];
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     polygonsStream = fetchPolygons();
-//     _zoomPanBehavior = MapZoomPanBehavior(enableDoubleTapZooming: true);
-//   }
-
-//   Stream<List<CustomPolygon>> fetchPolygons() async* {
-//     // Mantén un bucle infinito para buscar actualizaciones de polígonos continuamente
-//     while (true) {
-//       await Future.delayed(Duration(
-//           seconds: 10)); // Espera 10 segundos antes de buscar actualizaciones
-
-//       try {
-//         const url = 'http://192.168.100.123:8000/api/map';
-//         final response = await http.get(Uri.parse(url));
-//         if (response.statusCode == 200) {
-//           final List<dynamic> data = jsonDecode(response.body);
-//           List<CustomPolygon> polygons = [];
-//           for (var item in data) {
-//             final Map<String, dynamic> geojson = jsonDecode(item['geojson']);
-//             final List<dynamic> coordinates = geojson['coordinates'][0];
-//             List<LatLng> points = [];
-//             for (var coordinate in coordinates) {
-//               points.add(LatLng(coordinate[1], coordinate[0]));
-//             }
-//             CustomPolygon polygon = CustomPolygon(
-//               points: points,
-//               color: Color.fromARGB(114, 33, 149, 243),
-//               strokeColor: Color.fromARGB(83, 33, 149, 243),
-//               zone: item['zone'],
-//               days: item['days'],
-//               time: item['time'],
-//             );
-//             polygons.add(polygon);
-//           }
-//           yield polygons; // Emitir la lista de polígonos actualizada
-//         } else {
-//           throw Exception('Failed to fetch polygons from API');
-//         }
-//       } catch (e) {
-//         // Manejar cualquier error que ocurra al buscar polígonos
-//         print('Error al obtener polígonos: $e');
-//       }
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: StreamBuilder<List<CustomPolygon>>(
-//         stream: polygonsStream,
-//         builder: (context, snapshot) {
-//           if (snapshot.connectionState == ConnectionState.waiting) {
-//             return Center(child: CircularProgressIndicator());
-//           } else if (snapshot.hasError) {
-//             return Center(child: Text('Error: ${snapshot.error}'));
-//           } else {
-//             List<CustomPolygon> polygons = snapshot.data ?? [];
-//             return SfMaps(
-//               layers: [
-//                 MapTileLayer(
-//                   initialZoomLevel: 12,
-//                   initialFocalLatLng:
-//                       MapLatLng(-27.332474952498472, -55.864316516887556),
-//                   urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-//                   zoomPanBehavior: _zoomPanBehavior,
-//                   sublayers: [
-//                     MapPolygonLayer(
-//                       polygons: _polygons
-//                           .map((polygon) => MapPolygon(
-//                                 points: polygon.points
-//                                     .map((point) => MapLatLng(
-//                                         point.latitude, point.longitude))
-//                                     .toList(),
-//                                 color: polygon.color,
-//                                 strokeColor: polygon.strokeColor,
-//                                 onTap: () {
-//                                   _showPolygonInfo(context, polygon);
-//                                 },
-//                               ))
-//                           .toSet(),
-//                     ),
-//                   ],
-//                 ),
-//               ],
-//             );
-//           }
-//         },
-//       ),
-//     );
-//   }
-
-//   // Funcion para mostrar información del polígono al hacer click
-//   void _showPolygonInfo(BuildContext context, CustomPolygon polygon) {
-//     showModalBottomSheet(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return Container(
-//           padding: EdgeInsets.all(16.0),
-//           child: Column(
-//             crossAxisAlignment: CrossAxisAlignment.start,
-//             mainAxisSize: MainAxisSize.min,
-//             children: [
-//               Text(
-//                 'Itinerario del servicio',
-//                 style: TextStyle(
-//                   fontSize: 20,
-//                   fontWeight: FontWeight.bold,
-//                 ),
-//               ),
-//               SizedBox(height: 10),
-//               Text('Zona: ${polygon.zone}'),
-//               Text('Dias: ${polygon.days}'),
-//               Text('Horario: ${polygon.time}'),
-//             ],
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
-
-// class CustomPolygon {
-//   final List<LatLng> points;
-//   final Color color;
-//   final Color strokeColor;
-//   final String zone;
-//   final String days;
-//   final String time;
-
-//   CustomPolygon({
-//     required this.points,
-//     required this.color,
-//     required this.strokeColor,
-//     required this.zone,
-//     required this.days,
-//     required this.time,
-//   });
-// }
--- lib/src/news/news.dart
+++ lib/src/news/news.dart
@@ -1,128 +1,4 @@
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
-// import 'package:app_flutter/src/news_details/details.dart';
-// import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-// class NewsWidget extends StatefulWidget {
-//   const NewsWidget({super.key});
-
-//   @override
-//   State<NewsWidget> createState() => _NewsWidgetState();
-// }
-
-// class _NewsWidgetState extends State<NewsWidget> {
-//   @override
-//   void initState() {
-//     fetchNews();
-//     super.initState();
-//   }
-
-//   // Future<List<dynamic>> getNews() async {
-//   List<dynamic> news = [];
-
-//   void fetchNews() async {
-//     //String api_url = dotenv.get('API_URL', fallback: "");
-//     String api_url = "http://192.168.100.123:8000/api/";
-//     // String url = api_url + 'news';
-//     // final uri = Uri.parse(url);
-//     final response = await http.get(Uri.parse(api_url + 'news'));
-//     final body = response.body;
-//     final json = jsonDecode(body);
-//     setState(() {
-//       news = json;
-//       print(news);
-//     });
-//   }
-
-//   /// Builds the news screen widget.
-//   ///
-//   /// This method returns a Scaffold widget that displays a list of news articles.
-//   /// If the list of news is empty, a CircularProgressIndicator is shown in the center of the screen.
-//   /// Otherwise, a ListView.builder is used to display each news article using the postCard widget.
-//   ///
-//   /// The context parameter is the build context.
-//   /// The news parameter is the list of news articles.
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Noticias'),
-//         ),
-//         body: Container(
-//           child: news.isEmpty
-//               ? Container(
-//                   alignment: Alignment.center,
-//                   child: const CircularProgressIndicator(),
-//                 )
-//               : ListView.builder(
-//                   scrollDirection: Axis.vertical,
-//                   reverse: true,
-//                   itemCount: news.length,
-//                   itemBuilder: (context, index) {
-//                     return postCard(news, index);
-//                   },
-//                 ),
-//         ));
-//   }
-
-//   // Tarjeta de noticias
-//   Widget postCard(List<dynamic> news, int index) => GestureDetector(
-//         onTap: () {
-//           Navigator.push(
-//             context,
-//             MaterialPageRoute(
-//               builder: (context) => Details(
-//                 title: news[index]['title'],
-//                 description: news[index]['description'],
-//                 image: news[index]['picture'],
-//                 date: news[index]['created_at'],
-//                 // source: news[index]['source'],
-//                 // url: news[index]['url'],
-//               ),
-//             ),
-//           );
-//           print(news[index]);
-//         },
-//         child: Card(
-//             elevation: 10,
-//             margin: EdgeInsets.all(14),
-//             shape:
-//                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-//             child: Container(
-//               width: 300,
-//               padding: EdgeInsets.all(14),
-//               child: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: <Widget>[
-//                   Column(
-//                     children: <Widget>[
-//                       SizedBox(
-//                         height: 290,
-//                         width: 100,
-//                         child: Image.network(
-//                           news[index]['picture'],
-//                           fit: BoxFit.cover,
-//                         )
-//                       ),
-//                       // Image.network(
-//                       //   news[index]['picture'],
-//                       //   width: 300,
-//                       //   height: 100,
-//                       // ),
-//                       Text(
-//                         news[index]['title'],
-//                         style: TextStyle(
-//                             fontSize: 18, fontWeight: FontWeight.bold),
-//                         overflow: TextOverflow.ellipsis,
-//                       ),
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//             )),
-//       );
-// }
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -136,26 +12,28 @@ class NewsWidget extends StatefulWidget {
 }
 
 class _NewsWidgetState extends State<NewsWidget> {
+  late StreamController<List<dynamic>> _newsStreamController;
   late Stream<List<dynamic>> newsStream;
 
   @override
   void initState() {
     super.initState();
-    newsStream = fetchNews();
+    _newsStreamController = StreamController<List<dynamic>>();
+    newsStream = _newsStreamController.stream;
+    fetchNews();
   }
 
-  Stream<List<dynamic>> fetchNews() async* {
+  void fetchNews() async {
     // Mantén un bucle infinito para actualizar las noticias continuamente
     while (true) {
-      await Future.delayed(Duration(
-          seconds: 10)); // Espera 10 segundos antes de buscar nuevas noticias
+      await Future.delayed(Duration(seconds: 10));
 
       try {
         String api_url = "http://192.168.100.123:8000/api/";
         final response = await http.get(Uri.parse(api_url + 'news'));
         final body = response.body;
         final json = jsonDecode(body);
-        yield json; // Emitir la lista de noticias actualizada
+        _newsStreamController.add(json); // Emitir los datos al Stream
       } catch (e) {
         // Manejar cualquier error que ocurra al buscar las noticias
         print('Error al obtener noticias: $e');
@@ -235,8 +113,8 @@ class _NewsWidgetState extends State<NewsWidget> {
                 ),
                 const SizedBox(height: 8),
                 Text(
-                    /*"${news[index]['created_by']} · */"${news[index]['created_at']}",
-                    style: Theme.of(context).textTheme.bodySmall),
+                    /*"${news[index]['created_by']} · */ "${news[index]['created_at']}",
+                    style: Theme.of(context).textTheme.bodyText1),
                 const SizedBox(height: 8),
                 Row(
                   mainAxisSize: MainAxisSize.min,
@@ -270,6 +148,13 @@ class _NewsWidgetState extends State<NewsWidget> {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _newsStreamController
+        .close(); // Cerrar el controlador de Stream cuando se destruye el Widget
+    super.dispose();
   }
 }
 
